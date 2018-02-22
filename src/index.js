9bb42c4548af7f45df1e68e5cdf92564a3100ab8
Moonhead1902 <36646724+Moonhead1902@users.noreply.github.com>
2018-02-22 23:56:21 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  var b = base;
+  var counter = 0;
+  var arr = [];
+
+  for (var i = 2; i <= b ; i++) {
+    if (b % i === 0) {
+      for (var j = 2; j <= i; j++) {
+        if (i % j == 0) {
+          counter++;
+        }
+      }
+
+      if (counter === 1) {
+        arr.push(i);
+        b = b / i;
+        i--;
+      }
+    }
+
+    counter = 0;
+  }
+
+  var result = 0;
+  var i = 1;
+
+  while (Math.pow(arr[arr.length - 1], i) < number) {
+      result = result + Math.floor(number / Math.pow(arr[arr.length - 1], i));
+      i++;
+  }
+
+  return arr;
+
 }
